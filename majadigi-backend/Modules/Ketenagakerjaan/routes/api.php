a524910e2de4ad43a9c4bd7b216af54b014d9cbf
hernandoatha <hernandoatha@student.ub.ac.id>
2026-06-05 02:06:21 +0700

    deployment test

--- majadigi-backend/Modules/Ketenagakerjaan/routes/api.php
+++ majadigi-backend/Modules/Ketenagakerjaan/routes/api.php
@@ -9,31 +9,31 @@ use Modules\Ketenagakerjaan\Http\Controllers\TrainingController;
 use Modules\Ketenagakerjaan\Http\Controllers\TrainingParticipantController;
 use Modules\Ketenagakerjaan\Http\Controllers\TrainingCenterController;
 
-Route::middleware(['auth:sanctum'])->group(function () {
-
-    Route::prefix('ketenagakerjaan')->middleware(['auth:sanctum'])->group(function () {
-
-        Route::get('/training-centers', [TrainingCenterController::class, 'index']);
-        Route::get('/training-centers/{id}', [TrainingCenterController::class, 'show']);
-
-        // JOB
-        Route::get('/jobs', [JobTrainingController::class, 'index']);
-        Route::get('/jobs/{id}', [JobTrainingController::class, 'show']);
-
-        // APPLY
-        Route::post('/apply', [JobApplicationController::class, 'apply']);
-        Route::get('/my-applications', [JobApplicationController::class, 'myApplications']);
-
-        // TRAINING
-        Route::get('/trainings', [TrainingController::class, 'index']);
-        Route::get('/trainings/{id}', [TrainingController::class, 'show']);
-
-        // JOIN TRAINING
-        Route::post('/join-training', [TrainingParticipantController::class, 'join']);
-        Route::get('/my-trainings', [TrainingParticipantController::class, 'myTrainings']);
-        Route::get('/my-trainings/{id}', [TrainingParticipantController::class, 'show']);
-
-    });
+Route::middleware(['auth:sanctum'])->prefix('v1')->group(function () {
+    Route::apiResource('ketenagakerjaans', KetenagakerjaanController::class)->names('ketenagakerjaan');
 });
 
 
+//Route::prefix('ketenagakerjaan')->middleware(['auth:sanctum'])->group(function () {
+
+    Route::get('/training-centers', [TrainingCenterController::class, 'index']);
+    Route::get('/training-centers/{id}', [TrainingCenterController::class, 'show']);
+
+    // JOB
+    Route::get('/jobs', [JobTrainingController::class, 'index']);
+    Route::get('/jobs/{id}', [JobTrainingController::class, 'show']);
+
+    // APPLY
+    Route::post('/apply', [JobApplicationController::class, 'apply']);
+    Route::get('/my-applications', [JobApplicationController::class, 'myApplications']);
+
+    // TRAINING
+    Route::get('/trainings', [TrainingController::class, 'index']);
+    Route::get('/trainings/{id}', [TrainingController::class, 'show']);
+
+    // JOIN TRAINING
+    Route::post('/join-training', [TrainingParticipantController::class, 'join']);
+    Route::get('/my-trainings', [TrainingParticipantController::class, 'myTrainings']);
+    Route::get('/my-trainings/{id}', [TrainingParticipantController::class, 'show']);
+
+//});
